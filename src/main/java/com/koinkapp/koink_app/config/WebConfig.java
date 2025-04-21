3b4b30d1a6956bd1311407b119e50d2b1893815e
Juane Elizondo <juane.elizondo23@gmail.com>
2025-04-21 13:10:23 -0300

    modifique archivo WebConfig

--- src/main/java/com/koinkapp/koink_app/config/WebConfig.java
+++ src/main/java/com/koinkapp/koink_app/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "https://koinkapp.com/")
+                        .allowedOrigins("http://localhost:5173", "https://koinkapp.com", "https://www.koinkapp.com")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
