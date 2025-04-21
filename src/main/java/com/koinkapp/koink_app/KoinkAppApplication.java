2427c541f19be6eccc0e729f6a68d81369207f45
Juane Elizondo <juane.elizondo23@gmail.com>
2025-04-21 10:08:02 -0300

    backend arreglando error de carga .env

--- src/main/java/com/koinkapp/koink_app/KoinkAppApplication.java
+++ src/main/java/com/koinkapp/koink_app/KoinkAppApplication.java
@@ -11,8 +11,13 @@ public class KoinkAppApplication {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		dotenv.entries().forEach(entry -> {
+			if (System.getenv(entry.getKey()) == null) {
+				System.setProperty(entry.getKey(), entry.getValue());
+			}
+		});
 
 		SpringApplication.run(KoinkAppApplication.class, args);
 	}
